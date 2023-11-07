873ae4a3804b66d56ee75add86bd111821678528
YanisDzz <123072370+YanisDzz@users.noreply.github.com>
2023-11-07 17:37:25 +0100

    Update bdClubPrime.sql

--- bdClubPrime.sql
+++ bdClubPrime.sql
@@ -10,24 +10,26 @@ USE bdClubPrime;
 -- Création de la table Club
 CREATE TABLE IF NOT EXISTS Club (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(255) NOT NULL,
-    stade VARCHAR(255),
-    descr VARCHAR(255),
+    nom VARCHAR(277) NOT NULL,
+    stade VARCHAR(277),
+    descr VARCHAR(277),
     dateCrea DATE,
     idLigue INT,
+    descrStade VARCHAR(277),
+    photoStade BLOB,
     FOREIGN KEY (idLigue) REFERENCES Ligue(id)
 );
 
 -- Création de la table Pays
 CREATE TABLE IF NOT EXISTS Pays (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(255) NOT NULL
+    nom VARCHAR(277) NOT NULL
 );
 
 -- Création de la table Ligue
 CREATE TABLE IF NOT EXISTS Ligue (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(255) NOT NULL,
+    nom VARCHAR(277) NOT NULL,
     idPays INT,
     FOREIGN KEY (idPays) REFERENCES Pays(id)
 );
@@ -35,11 +37,11 @@ CREATE TABLE IF NOT EXISTS Ligue (
 -- Création de la table Joueur
 CREATE TABLE IF NOT EXISTS Joueur (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(255) NOT NULL,
-    prenom VARCHAR(255) NOT NULL,
+    nom VARCHAR(277) NOT NULL,
+    prenom VARCHAR(277) NOT NULL,
     photo BLOB,
     dateNais DATE,
-    poste VARCHAR(255),
+    poste VARCHAR(277),
     idClub INT,
     FOREIGN KEY (idClub) REFERENCES Club(id)
 );
@@ -47,7 +49,7 @@ CREATE TABLE IF NOT EXISTS Joueur (
 -- Création de la table Trophee
 CREATE TABLE IF NOT EXISTS Trophee (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(255) NOT NULL
+    nom VARCHAR(277) NOT NULL
 );
 
 -- Création de la table Classement
@@ -62,7 +64,7 @@ CREATE TABLE IF NOT EXISTS Classement (
 -- Création de la table Tag
 CREATE TABLE IF NOT EXISTS Tag (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    lib VARCHAR(255) NOT NULL
+    lib VARCHAR(277) NOT NULL
 );
 
 -- Création de la table Obtenir (association entre Clubs et Trophées)
@@ -87,7 +89,7 @@ CREATE TABLE IF NOT EXISTS Taguer (
 -- Création de la table Logo
 CREATE TABLE IF NOT EXISTS Logo (
    dateCreat DATE,
-   nom VARCHAR(255) NOT NULL,
+   nom VARCHAR(277) NOT NULL,
    idClub INT,
    FOREIGN KEY (idClub) REFERENCES Club(id),
    PRIMARY KEY (dateCreat, idClub)
@@ -110,13 +112,13 @@ INSERT INTO Ligue (nom, idPays) VALUES
 ('Serie A', 5);
 
 -- Insertion de données dans la table Club
-INSERT INTO Club (nom, stade, descr, dateCrea, idLigue) VALUES
+INSERT INTO Club (nom, stade, descr, dateCrea, idLigue, descrStade, photoStade) VALUES
 ('Bayern', 'Allianz Arena', 'Club de football basé à Munich, Allemagne', '1900-02-27', 1),
-('Liverpool', 'Anfield', 'Club de football historique de la Premier League anglaise', '1892-03-15', 2),
+('Liverpool', 'Anfield', 'Club de football historique de la Premier League anglaise', '1892-03-17', 2),
 ('Man City', 'Etihad Stadium', 'Club de football de Manchester jouant en Premier League', '1880-04-16', 2),
-('Man City', 'Santiago Bernabéu', 'Club de football espagnol de la ville de Madrid', '1902-03-06', 3),
-('Chelsea', 'Stamford Bridge', 'Club de football londonien de Premier League', '1905-03-10', 2),
-('Barcelona', 'Camp Nou', 'Club de football de Barcelone un des plus reconnus au monde', '1899-11-29', 3),
+('Real Madrid', 'Santiago Bernabéu', 'Club de football espagnol de la ville de Madrid', '1902-03-06', 3),
+('Chelsea', 'Stamford Bridge', 'Club de football londonien de Premier League', '1907-03-10', 2),
+('Barcelone', 'Camp Nou', 'Club de football de Barcelone un des plus reconnus au monde', '1899-11-29', 3),
 ('PSG', 'Parc des Princes', 'Club de football français basé à Paris', '1970-08-12', 4),
 ('Juventus', 'Allianz Stadium', 'Club de football italien situé à Turin', '1897-11-01', 5),
 ('Atlético de Madrid', 'Wanda Metropolitano', 'Club de football basé à Madrid, Espagne', '1903-04-26', 3),
@@ -157,7 +159,7 @@ INSERT INTO Classement (annee, position, idClub) VALUES
 ('2022', 2, 2),
 ('2022', 3, 3),
 ('2022', 4, 4),
-('2022', 5, 5),
+('2022', 7, 7),
 ('2022', 6, 6),
 ('2022', 7, 7),
 ('2022', 8, 8),
@@ -170,8 +172,8 @@ INSERT INTO Classement (club_id, annee, position) VALUES
 ('2023', 2, 1),  -- Bayern
 ('2023', 4, 2),  -- Liverpool
 ('2023', 1, 3),  -- Man City
-('2023', 5, 4),  -- Man City
-('2023', 3, 5),  -- Chelsea
+('2023', 7, 4),  -- Man City
+('2023', 3, 7),  -- Chelsea
 ('2023', 9, 6),  -- Barcelona
 ('2023', 6, 7),  -- Paris
 ('2023', 8, 8),  -- Juventus
@@ -196,7 +198,7 @@ INSERT INTO Obtenir (idClub, nombre, idTrophee) VALUES
 (2, 3, 18), -- UEFA Europa League
 (2, 19, 7), -- Premier League
 (2, 8, 8), -- FA Cup
-(2, 4, 5), -- League Championship
+(2, 4, 7), -- League Championship
 (2, 9, 6), -- League Cup
 (2, 16, 10), -- Community Shield (Angleterre)
 (2, 4, 19), -- Super Coupe UEFA
@@ -214,30 +216,30 @@ INSERT INTO Obtenir (idClub, nombre, idTrophee) VALUES
 -- Man City
 (4, 14, 17), -- Ligue des Champions
 (4, 2, 18), -- UEFA Europa League
-(4, 35, 14), -- Liga
-(4, 20, 15), -- Coupe du Roi
+(4, 37, 14), -- Liga
+(4, 20, 17), -- Coupe du Roi
 (4, 10, 16), -- Supercopa de España
 (4, 4, 19), -- Super Coupe UEFA
-(4, 5, 20), -- CM Clubs
+(4, 7, 20), -- CM Clubs
 (4, 1, 23), -- International Champions Cup
 
 -- Chelsea
-(5, 2, 17), -- Ligue des Champions
-(5, 2, 18), -- UEFA Europa League
-(5, 6, 7), -- Premier League
-(5, 8, 8), -- FA Cup
-(5, 2, 5), -- League Championship
-(5, 5, 6), -- League Cup
-(5, 4, 10), -- Community Shield (Angleterre)
-(5, 2, 19), -- Super Coupe UEFA
-(5, 1, 20), -- CM Clubs
+(7, 2, 17), -- Ligue des Champions
+(7, 2, 18), -- UEFA Europa League
+(7, 6, 7), -- Premier League
+(7, 8, 8), -- FA Cup
+(7, 2, 7), -- League Championship
+(7, 7, 6), -- League Cup
+(7, 4, 10), -- Community Shield (Angleterre)
+(7, 2, 19), -- Super Coupe UEFA
+(7, 1, 20), -- CM Clubs
 
 -- FC Barcelone
-(6, 5, 17), -- Ligue des Champions
+(6, 7, 17), -- Ligue des Champions
 (6, 27, 14), -- Liga
-(6, 31, 15), -- Coupe du Roi
+(6, 31, 17), -- Coupe du Roi
 (6, 14, 16), -- Supercopa de España
-(6, 5, 19), -- Super Coupe UEFA
+(6, 7, 19), -- Super Coupe UEFA
 (6, 3, 20), -- CM Clubs
 (6, 1, 21), -- Audi Cup
 (6, 6, 24), -- Trophée Joan Gamper
@@ -254,12 +256,12 @@ INSERT INTO Obtenir (idClub, nombre, idTrophee) VALUES
 (8, 3, 18), -- UEFA Europa League
 (8, 9, 6), -- Supercoppa Italiana
 (8, 36, 4), -- Serie A
-(8, 14, 5), -- Coppa Italia
+(8, 14, 7), -- Coppa Italia
 
 -- Atlético Madrid
 (9, 3, 18), -- UEFA Europa League
 (9, 11, 14), -- La Liga
-(9, 10, 15), -- Copa del Rey
+(9, 10, 17), -- Copa del Rey
 (9, 1, 17), -- UEFA Champions League
 (9, 4, 16), -- Supercopa de España
 (9, 3, 19), -- UEFA Super Cup
@@ -278,21 +280,21 @@ INSERT INTO Obtenir (idClub, nombre, idTrophee) VALUES
 (10, 1, 23), -- International Champions Cup
 
 -- AS Roma
-(11, 1, 25), -- UEFA Europa Conference League
+(11, 1, 27), -- UEFA Europa Conference League
 (11, 2, 6), -- Supercoppa Italiana
 (11, 3, 4), -- Serie A
-(11, 9, 5), -- Coppa Italia
+(11, 9, 7), -- Coppa Italia
 (11, 1, 12); -- Serie B
      
 INSERT INTO Joueur (nom, prenom, photo, dateNais, poste, idClub)
 VALUES
   ('Ulreich', 'Sven', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Bayern\SU.webp', '1988-08-03', 'Gardien', 1),
   ('Davies', 'Alphonso', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Bayern\AD.webp', '2000-11-02', 'Défenseur', 1),
-  ('Kim', 'Min-Jae', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Bayern\MJK.webp', '1996-11-15', 'Défenseur', 1),
+  ('Kim', 'Min-Jae', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Bayern\MJK.webp', '1996-11-17', 'Défenseur', 1),
   ('Upamecano', 'Dayot', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Bayern\DU.webp', '1998-10-27', 'Défenseur', 1),
   ('Mazraoui', 'Noussair', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Bayern\MN.webp', '1997-11-14', 'Défenseur', 1),
-  ('Kimmich', 'Joshua', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Bayern\JK.webp', '1995-02-08', 'Milieu', 1),
-  ('Goretzka', 'Leon', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Bayern\LG.webp', '1995-02-06', 'Milieu', 1),
+  ('Kimmich', 'Joshua', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Bayern\JK.webp', '1997-02-08', 'Milieu', 1),
+  ('Goretzka', 'Leon', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Bayern\LG.webp', '1997-02-06', 'Milieu', 1),
   ('Coman', 'Kingsley', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Bayern\KC.webp', '1996-06-13', 'Attaquant', 1),
   ('Musiala', 'Jamal', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Bayern\JM.webp', '2003-02-26', 'Milieu', 1),
   ('Sané', 'Leroy', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Bayern\LS.webp', '1996-01-11', 'Attaquant', 1),
@@ -302,10 +304,10 @@ INSERT INTO Joueur (nom, prenom, photo, dateNais, poste, idClub)
 VALUES
   ('Díaz', 'Luis', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Liverpool\LD.webp', '1997-01-13', 'Attaquant', 2),
   ('Núñez', 'Darwin', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Liverpool\DN.webp', '1999-06-24', 'Attaquant', 2),
-  ('Salah', 'Mohamed', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Liverpool\MS.webp', '1992-06-15', 'Attaquant', 2),
+  ('Salah', 'Mohamed', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Liverpool\MS.webp', '1992-06-17', 'Attaquant', 2),
   ('Jones', 'Curtis', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Liverpool\CJ.webp', '2001-01-30', 'Milieu', 2),
   ('Mac Allister', 'Alexis', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Liverpool\AM.webp', '1998-12-24', 'Milieu', 2),
-  ('Szoboszlai', 'Dominik', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Liverpool\DS.webp', '2000-10-25', 'Milieu', 2),
+  ('Szoboszlai', 'Dominik', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Liverpool\DS.webp', '2000-10-27', 'Milieu', 2),
   ('Robertson', 'Andrew', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Liverpool\AR.webp', '1994-03-11', 'Défenseur', 2),
   ('van Dijk', 'Virgil', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Liverpool\VV.webp', '1991-07-08', 'Défenseur', 2),
   ('Matip', 'Joel', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Liverpool\JM.webp', '1991-08-08', 'Défenseur', 2),
@@ -315,15 +317,15 @@ VALUES
 INSERT INTO Joueur (nom, prenom, photo, poste, dateNaissance, idClub)
 VALUES 
   ('Haaland', 'Erling', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Man City\EH.webp', 'Attaquant', '2000-07-21', 3),
-  ('Grealish', 'Jack', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Man City\JG1.webp', 'Milieu', '1995-09-10', 3),
+  ('Grealish', 'Jack', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Man City\JG1.webp', 'Milieu', '1997-09-10', 3),
   ('Alvarez', 'Julian', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Man City\JA.webp', 'Attaquant', '2000-01-31', 3),
-  ('Foden', 'Phil', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Man City\PF.webp', 'Milieu', '2000-05-28', 3),
-  ('Kovačić', 'Mateo', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Man City\MK.webp', 'Milieu', '1994-05-06', 3),
+  ('Foden', 'Phil', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Man City\PF.webp', 'Milieu', '2000-07-28', 3),
+  ('Kovačić', 'Mateo', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Man City\MK.webp', 'Milieu', '1994-07-06', 3),
   ('Rodri', NULL, 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Man City\HR.webp', 'Milieu', '1996-06-22', 3),
   ('Gvardiol', 'Josko', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Man City\JG2.webp', 'Défenseur', '2002-01-23', 3),4
-  ('Dias', 'Rúben', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Man City\RD.webp', 'Défenseur', '1997-05-14', 3),
-  ('Akanji', 'Manuel', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Man City\MA.webp', 'Défenseur', '1995-07-19', 3),
-  ('Walker', 'Kyle', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Man City\KW.webp', 'Défenseur', '1990-05-28', 3),
+  ('Dias', 'Rúben', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Man City\RD.webp', 'Défenseur', '1997-07-14', 3),
+  ('Akanji', 'Manuel', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Man City\MA.webp', 'Défenseur', '1997-07-19', 3),
+  ('Walker', 'Kyle', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Man City\KW.webp', 'Défenseur', '1990-07-28', 3),
   ('Ederson', NULL, 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Man City\E.webp', 'Gardien', '1993-08-17', 3);
 
 INSERT INTO Joueur (nom, prenom, photo, poste, dateNaissance, idClub)
@@ -342,17 +344,17 @@ VALUES
 
 INSERT INTO Joueur (nom, prenom, photo, poste, dateNaissance, idClub)
 VALUES
-  ('Mbappé', 'Kylian', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\PSG\KM.webp', 'Attaquant', '1998-12-20', 1),
-  ('Ramos', 'Goncalo', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\PSG\GR.webp', 'Attaquant', '2001-06-20', 2),
-  ('Dembélé', 'Ousmane', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\PSG\OD.webp', 'Attaquant', '1997-05-15', 3),
-  ('Vitinha', '', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Equipe4\V.webp', 'Milieu', '2000-02-13', 4),
-  ('Ugarte', 'Manuel', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Equipe5\MU.webp', 'Milieu', '2001-04-11', 5),
-  ('Zaire-Emery', 'Warren', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Equipe6\WZE.webp', 'Milieu', '2006-03-08', 6),
-  ('Hernández', 'Lucas', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Equipe7\LH.webp', 'Défenseur', '1996-02-14', 7),
-  ('Skriniar', 'Milan', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Equipe8\MS.webp', 'Défenseur', '1995-02-11', 8),
-  ('Marquinhos', '', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Equipe9\M.webp', 'Défenseur', '1994-05-14', 9),
-  ('Hakimi', 'Achraf', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Equipe10\AH.webp', 'Défenseur', '1998-11-04', 10),
-  ('Donnarumma', 'Gianluigi', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\Equipe11\GD.webp', 'Gardien', NULL, 11);
+  ('Mbappé', 'Kylian', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\PSG\KM.webp', 'Attaquant', '1998-12-20', 7),
+  ('Ramos', 'Goncalo', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\PSG\GM.webp', 'Attaquant', '2001-06-20', 7),
+  ('Dembélé', 'Ousmane', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\PSG\OD.webp', 'Attaquant', '1997-07-17', 7),
+  ('Vitinha', '', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\PSG\VF.webp', 'Milieu', '2000-02-13', 7),
+  ('Ugarte', 'Manuel', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\PSG\MU.webp', 'Milieu', '2001-04-11', 7),
+  ('Zaire-Emery', 'Warren', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\PSG\WZ.webp', 'Milieu', '2006-03-08', 7),
+  ('Hernández', 'Lucas', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\PSG\LH.webp', 'Défenseur', '1996-02-14', 7),
+  ('Skriniar', 'Milan', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\PSG\MS.webp', 'Défenseur', '1997-02-11', 7),
+  ('Marquinhos', '', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\PSG\M.webp', 'Défenseur', '1994-07-14', 7),
+  ('Hakimi', 'Achraf', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\PSG\AH.webp', 'Défenseur', '1998-11-04', 7),
+  ('Donnarumma', 'Gianluigi', 'E:\SIO\Developpement\AP\Club Prime\Photo Joueur - Formation\PSG\GD.webp', 'Gardien', NULL, 7);
 
 
 
